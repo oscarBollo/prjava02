4ccbd53609f5fa4ae0be7f9efc9a3c8147b6a9de
Alumne <Alumne@192.168.17.18>
2014-04-24 10:57:10 +0200

    primera versio de prjava02.

---
+++ src/prjava02/Prjava02.java
@@ -0,0 +1,18 @@
+package prjava02;
+
+import java.io.*; import java.net.*;
+
+public class Prjava02 {
+    public static void main(String[] args){
+        try {
+            InetAddress addr = InetAddress.getLocalHost(); byte[] ipAddr = addr.getAddress();
+            String hostname = addr.getHostName(); System.out.println("hostname="+hostname);
+            System.out.println("Nom de l'usuari: " + System.getProperty("user.name"));
+            System.out.println("Carpeta Personal: " + System.getProperty("user.home"));
+            System.out.println("Sistema operatiu: " + System.getProperty("os.name"));
+            System.out.println("Versió OS: " + System.getProperty("os.version"));
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
